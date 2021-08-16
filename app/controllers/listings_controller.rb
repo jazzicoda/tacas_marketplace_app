df42a445ea3f82a2948cbe6b685e9f4accbcab8a
Jasmine Ofaolain <jasmineofaolain@gmail.com>
2021-08-17 00:11:25 +1000

    Styled landing page with Image and SCSS Flexbox

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,7 +6,7 @@ class ListingsController < ApplicationController
 
   # GET /listings or /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.all.includes(:category)
   end
 
   # GET /listings/1 or /listings/1.json
